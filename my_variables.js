a153186c5f29518141a2edb04cc9424851fa0bf5
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-03 05:19:38 -0400

    i have added a git repository into  variable_intro

---
+++ my_variables.js
@@ -0,0 +1,18 @@
+var sunshine = true;
+var age = 21;
+var surname ='isaacs';
+var date = new Date();
+var fruit = ['apple', 'banana'];
+
+
+console.log(sunshine);
+console.log(typeof sunshine);
+
+console.log (age);
+console.log(typeof age);
+console.log (surname);
+console.log (typeof surname);
+console.log (date);
+console.log (typeof date);
+console.log (fruit);
+console.log (typeof fruit);
